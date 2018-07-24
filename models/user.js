5a07a15310392c82a3b204d5f615148737726b71
cmronk <christinemronk@gmail.com>
2018-07-24 17:02:56 -0500

    updating app

--- models/user.js
+++
@@ -1,9 +0,0 @@
-module.exports = function(sequelize, DataTypes) {
-  var User = sequelize.define("User", {
-    firstname: DataTypes.STRING,
-    lastname: DataTypes.STRING,
-    username: DataTypes.STRING,
-    email: DataTypes.STRING
-  });
-  return User;
-};
