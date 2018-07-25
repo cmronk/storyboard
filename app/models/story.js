50d921e448458b07221e0d934fd5905ddc549229
TheBigHit <36009324+Thebighit21@users.noreply.github.com>
2018-07-25 18:01:51 -0500

    Updated Storyboards table with Genre column. Connected to index.handlebars. Started updating external links under genres sections

--- app/models/story.js
+++ app/models/story.js
@@ -2,7 +2,8 @@ module.exports = function(sequelize, DataTypes) {
   var Storyboard = sequelize.define("Storyboard", {
     characterList: DataTypes.STRING,
     imageurl: DataTypes.TEXT,
-    story: DataTypes.TEXT
+    story: DataTypes.TEXT,
+    genre: DataTypes.TEXT
   });
   return Storyboard;
 };
--- server.js
+++ server.js
@@ -14,6 +14,7 @@ if (process.env.JAWSDB_URL) {
     host: "localhost",
     user: "root",
     password: "root",
+    port: 8889,
     database: "storyboardDB"
   });
 }
