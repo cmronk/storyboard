7ab4664d5295d8640cd6032f8d1b5bb180c98f28
cmronk <christinemronk@gmail.com>
2018-07-28 03:44:21 -0500

    removed socet for heroku

--- server.js
+++ server.js
@@ -85,51 +85,51 @@ db.sequelize.sync(syncOptions).then(function() {
   });
 });
 
-// socket logic
-var server = require("http").createServer(app);
-var io = require("socket.io").listen(server);
+// // socket logic
+// var server = require("http").createServer(app);
+// var io = require("socket.io").listen(server);
 
-users = [];
-connections = [];
+// users = [];
+// connections = [];
 
-server.listen(process.env.PORT || 4000);
-console.log("server running");
-app.use(express.static("public"));
+// server.listen(process.env.PORT || 4000);
+// console.log("server running");
+// app.use(express.static("public"));
 
-// app.get("/", function (req, res) {
-//     res.sendFile(__dirname + "/index.html");
+// // app.get("/", function (req, res) {
+// //     res.sendFile(__dirname + "/index.html");
+// // });
+
+// io.sockets.on("connection", function (socket) {
+//   connections.push(socket);
+//   console.log("connected: %s sockets connected", connections.length);
+
+//   // disconnect
+//   socket.on("disconnect", function (data) {
+//       users.splice(users.indexOf(socket.username), 1);
+//       updateUsernames();
+//       connections.splice(connections.indexOf(socket), 1);
+//       console.log("Disconnected: %s sockets connected", connections.length);
+//   });
+
+//   // send message
+//   socket.on("send message", function(data){
+//       console.log(data);
+//       io.sockets.emit("new message", {msg: data, user: socket.username});
+//   });
+
+//   // new user
+//   socket.on("new user", function(data, callback){
+//       callback(true);
+//       socket.username = data;
+//       users.push(socket.username);
+//       updateUsernames();
+//   });
+
+//   function updateUsernames(){
+//       io.sockets.emit("get users", users);
+//   }
 // });
-
-io.sockets.on("connection", function (socket) {
-  connections.push(socket);
-  console.log("connected: %s sockets connected", connections.length);
-
-  // disconnect
-  socket.on("disconnect", function (data) {
-      users.splice(users.indexOf(socket.username), 1);
-      updateUsernames();
-      connections.splice(connections.indexOf(socket), 1);
-      console.log("Disconnected: %s sockets connected", connections.length);
-  });
-
-  // send message
-  socket.on("send message", function(data){
-      console.log(data);
-      io.sockets.emit("new message", {msg: data, user: socket.username});
-  });
-
-  // new user
-  socket.on("new user", function(data, callback){
-      callback(true);
-      socket.username = data;
-      users.push(socket.username);
-      updateUsernames();
-  });
-
-  function updateUsernames(){
-      io.sockets.emit("get users", users);
-  }
-});
 
 connection.connect();
 module.exports = connection;
