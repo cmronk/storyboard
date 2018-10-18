0e9c03f294266e1664bb871d759baa8b7c4b21a2
cmronk <christinemronk@gmail.com>
2018-10-18 11:37:35 -0500

    reverted to previous version

--- server.js
+++ server.js
@@ -87,36 +87,42 @@ db.sequelize.sync(syncOptions).then(function() {
 // socket logic
 var server = require("http").createServer(app);
 var io = require("socket.io").listen(server);
- users = [];
+
+users = [];
 connections = [];
- server.listen(process.env.PORT || 5000);
-console.log("server running");
- app.get("/", function (req, res) {
-    res.sendFile(__dirname + "/index.html");
+
+
+app.get("/", function (req, res) {
+    path.join(__dirname, "../views");
 });
- io.sockets.on("connection", function (socket) {
+
+io.sockets.on("connection", function (socket) {
     connections.push(socket);
     console.log("connected: %s sockets connected", connections.length);
-     // disconnect
+
+    // disconnect
     socket.on("disconnect", function (data) {
         users.splice(users.indexOf(socket.username), 1);
         updateUsernames();
         connections.splice(connections.indexOf(socket), 1);
         console.log("Disconnected: %s sockets connected", connections.length);
     });
-     // send message
+
+    // send message
     socket.on("send message", function(data){
         console.log(data);
         io.sockets.emit("new message", {msg: data, user: socket.username});
     });
-     // new user
+
+    // new user
     socket.on("new user", function(data, callback){
         callback(true);
         socket.username = data;
         users.push(socket.username);
         updateUsernames();
     });
-     function updateUsernames(){
+
+    function updateUsernames(){
         io.sockets.emit("get users", users);
     }
 });
